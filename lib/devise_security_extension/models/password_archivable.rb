927617550d01eb9bac3e5e68f2cde111cb70b78b
Marco Scholl <develop@marco-scholl.de>
2011-02-13 08:53:27 +0100

    fix attr

--- lib/devise_security_extension/models/password_archivable.rb
+++ lib/devise_security_extension/models/password_archivable.rb
@@ -18,7 +18,7 @@ module Devise # :nodoc:
       module InstanceMethods # :nodoc:
 
         def validate_password_archive
-          self.errors.add(attr, :taken_in_past) if password_archive_included?
+          self.errors.add(:password, :taken_in_past) if password_archive_included?
         end
 
         # validate is the password used in the past
